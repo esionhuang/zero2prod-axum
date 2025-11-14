1bcd71575d9d9b1b98efd15dc11f6c0a089d527d
EthanHuang <it-09@ace-tec.com>
2025-11-14 16:22:30 +0800

    添加 cconfigurations 结构,并实现从 configuration.yaml 文件读取配置

--- src/main.rs
+++ src/main.rs
@@ -1,11 +1,11 @@
 use tokio::net::TcpListener;
-use zero2prod_axum::run;
+use zero2prod_axum::{configuration::get_configuration, run};
 
 #[tokio::main]
 async fn main() -> Result<(), std::io::Error> {
-    let listener = TcpListener::bind("0.0.0.0:13000").await?;
-
+    let configuration = get_configuration().expect("Failed to read configuration.");
+    let address = format!("127.0.0.1:{}", configuration.application_port);
+    let listener = TcpListener::bind(address).await?;
     run(listener).await?.await?;
-
     Ok(())
 }
--- tests/health_check.rs
+++ tests/health_check.rs
@@ -1,5 +1,6 @@
+use sqlx::{Connection, PgConnection};
 use tokio::net::TcpListener;
-use zero2prod_axum::run;
+use zero2prod_axum::{configuration::get_configuration, run};
 
 async fn spawn_app() -> String {
     let listener = TcpListener::bind("0.0.0.0:0")
@@ -34,6 +35,16 @@ async fn health_check_work() {
 #[tokio::test]
 async fn subscribe_returns_a_200_for_valid_form_data() {
     let app_address = spawn_app().await;
+
+    let configuration_string = get_configuration()
+        .expect("Failed to read configuration")
+        .database
+        .connection_string();
+
+    let mut connection = PgConnection::connect(&configuration_string)
+        .await
+        .expect("Failed to connect to Postgres.");
+
     let client = reqwest::Client::new();
 
     let body = "name=le%20guin&email=ursula_le_guin%40gmail.com";
@@ -47,6 +58,14 @@ async fn subscribe_returns_a_200_for_valid_form_data() {
 
     // Assert
     assert_eq!(200, response.status().as_u16());
+
+    let saved = sqlx::query!("SELECT email, name FROM subscriptions",)
+        .fetch_one(&mut connection)
+        .await
+        .expect("Failed to fetch saved subscription.");
+
+    assert_eq!(saved.email, "ursula_le_guin@gmail.com");
+    assert_eq!(saved.name, "le guin");
 }
 
 // 订阅数据不全时,返回400错误
