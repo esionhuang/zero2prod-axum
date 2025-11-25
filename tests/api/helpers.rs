bce805307ebdb5157108b92684e85acbb0b7615b
EthanHuang <it-09@ace-tec.com>
2025-11-25 15:07:43 +0800

    引用 argon2 实现密码带可选负载配置计算

--- tests/api/helpers.rs
+++ tests/api/helpers.rs
@@ -1,5 +1,8 @@
+use argon2::{
+    Argon2, PasswordHasher,
+    password_hash::{SaltString, rand_core::OsRng},
+};
 use once_cell::sync::Lazy;
-use sha3::Digest;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use wiremock::MockServer;
@@ -8,7 +11,7 @@ use zero2prod_axum::{
     configuration::{DatabaseSettings, get_configuration},
     get_connection_pool,
     temeletry::{get_subscriber, init_subscriber},
-};
+}; // 或使用 thread_rng
 
 // 使用 once_cell 的懒加载定义遥测实现,确保在多个测试并发执行时只初始化一次遥测实现
 static TRACING: Lazy<()> = Lazy::new(|| {
@@ -41,8 +44,12 @@ impl TestUser {
 
     /// 存储测试用户
     async fn store(&self, pool: &PgPool) {
-        let password_hash = sha3::Sha3_256::digest(self.password.as_bytes());
-        let password_hash = format!("{:x}", password_hash);
+        let salt = SaltString::generate(&mut OsRng);
+        let password_hash = Argon2::default()
+            .hash_password(self.password.as_bytes(), &salt)
+            .unwrap()
+            .to_string();
+
         sqlx::query!(
             "INSERT INTO users (user_id, username, password_hash) VALUES ($1, $2, $3)",
             self.user_id,
